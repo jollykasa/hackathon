60c57aef925c145b58ad02c909c80b76f5534b88
niraj.mhrjn770 <niraj.mhrjn770@gmail.com>
2024-01-20 15:11:23 +0545

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,23 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
-bool _iconBool=false;
-IconData _iconLight=Icons.wb_sunny;
-IconData _iconDark=Icons.nights_stay;
 
-ThemeData _lightTheme= ThemeData(
-  primarySwatch:Colors.deepPurple,
-  brightness:Brightness.light,
-  colorScheme: ColorScheme.light(),
-  buttonTheme: const ButtonThemeData(buttonColor: Colors.blueAccent)
-);
-ThemeData _darkTheme= ThemeData(
-  primarySwatch:Colors.blueGrey,
-  brightness:Brightness.dark,
+bool _iconBool = false;
+IconData _iconLight = Icons.wb_sunny;
+IconData _iconDark = Icons.nights_stay;
+
+ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.deepPurple,
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(),
+    buttonTheme: const ButtonThemeData(buttonColor: Colors.blueAccent));
+ThemeData _darkTheme = ThemeData(
+  primarySwatch: Colors.blueGrey,
+  brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(),
   // 7
 );
+
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
@@ -33,7 +34,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: _iconBool? _darkTheme:_lightTheme,
+      theme: _iconBool ? _darkTheme : _lightTheme,
       home: Scaffold(
         appBar: AppBar(
           title: Row(
@@ -42,31 +43,38 @@ class _MyAppState extends State<MyApp> {
               SizedBox(
                 width: 30,
                 height: 30,
-                child:Image.asset('assets/images/rem.png'),
+                child: Image.asset('assets/images/rem.png'),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 30,
               ),
-              const Center(child: Text("Care Alert",style: TextStyle(fontSize: 20),)),
+              const Center(
+                  child: Text(
+                "Care Alert",
+                style: TextStyle(fontSize: 20),
+              )),
             ],
           ),
           actions: [
-            IconButton(onPressed: (){
-                setState(() {
-                  _iconBool=!_iconBool;
-                });
-            },
-                icon: Icon(_iconBool? _iconDark:_iconLight))
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _iconBool = !_iconBool;
+                  });
+                },
+                icon: Icon(_iconBool ? _iconDark : _iconLight))
           ],
         ),
-        body:Container(
-         child: Center(child: Text("Hello",style: TextStyle(fontSize: 30),)),
-        ) ,
+        body: const Center(
+            child: Text(
+          "Hello",
+          style: TextStyle(fontSize: 30),
+        )),
         //floatingActionButtonLocation:FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
+          onPressed: () {},
           backgroundColor: Colors.grey,
-          child: const Icon(Icons.add,size: 30),
+          child: const Icon(Icons.add, size: 30),
         ),
       ),
     );
