02b26812c0167d87da456f9ea566c0aad389e898
niraj.mhrjn770 <niraj.mhrjn770@gmail.com>
2024-01-21 10:49:45 +0545

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,20 @@ import 'package:care_alert/alarm_clock/view/alarm%20view/alarm_view.dart';
 import 'package:care_alert/alarm_clock/view/splash_view/splash_screen.dart';
 import 'package:care_alert/home.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter/services.dart';
-=======
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
->>>>>>> b31bd00082bf91d568911b89cf96baa4c1bf69e8
 import 'home.dart';
 import 'SplashScreen.dart';
 import 'myheaderdrawer.dart';
 import 'setting.dart';
 import 'history.dart';
 
-<<<<<<< HEAD
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  await Alarm.init();
-=======
-void main() async{
   // await AndroidAlarmManager.initialize();
   // WidgetsFlutterBinding.ensureInitialized();
   //
   // // Initialize the notification plugin
   // await initNotifications();
 
->>>>>>> b31bd00082bf91d568911b89cf96baa4c1bf69e8
   runApp(const MyApp());
 }
 // Future<void> initNotifications() async {
